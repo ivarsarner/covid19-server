f5b23375eddcad1fffde72e3dcd8372bc652985a
ivarsarner <ivarsarner@gmail.com>
2021-02-11 19:20:28 +0100

    fixed faulty api call

--- api/api.js
+++ api/api.js
@@ -5,37 +5,34 @@ const dateToday = moment().subtract(2, 'days').format('YYYYMMDD');
 const datePrevious = moment().subtract(4, 'days').format('YYYYMMDD');
 
 const fetchData = async (date) => {
-	try {
-		const { data } = await axios.get(
-			`https://covidtracking.com/api/v1/states/${date}.json`
-		);
-		return data;
-	} catch (error) {
-		console.error(error);
-	}
+  console.log(date);
+  const { data } = await axios.get(
+    `https://api.covidtracking.com/v1/states/current.json`
+  );
+  return data;
 };
 
 const calculateNewDeaths = (todayData, oldData) => {
-	return todayData.map((item, index) => ({
-		state: item.state,
-		deaths: item.death - oldData[index].death,
-	}));
+  return todayData.map((item, index) => ({
+    state: item.state,
+    deaths: item.death - oldData[index].death,
+  }));
 };
 
 const aggregateData = (todayData, newDeathsData) => {
-	return todayData.map((item, index) => ({
-		state: item.state,
-		hospitalizedCurrently: item.hospitalizedCurrently || 0,
-		deaths: newDeathsData[index].deaths,
-	}));
+  return todayData.map((item, index) => ({
+    state: item.state,
+    hospitalizedCurrently: item.hospitalizedCurrently || 0,
+    deaths: newDeathsData[index].deaths,
+  }));
 };
 
 const getCovidData = async () => {
-	const todayData = await fetchData(dateToday);
-	const previousData = await fetchData(datePrevious);
-	const newDeathsData = calculateNewDeaths(todayData, previousData);
+  const todayData = await fetchData(dateToday);
+  const previousData = await fetchData(datePrevious);
+  const newDeathsData = calculateNewDeaths(todayData, previousData);
 
-	return aggregateData(todayData, newDeathsData);
+  return aggregateData(todayData, newDeathsData);
 };
 
 module.exports = { getCovidData, calculateNewDeaths, aggregateData };
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const { getCovidData } = require('../api/api');
 
 router.get('/covid', async (req, res) => {
-	const covidData = await getCovidData();
-	res.json(covidData);
+  try {
+    const covidData = await getCovidData();
+    console.log(covidData);
+    res.json(covidData);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ error: error.message });
+  }
 });
 
 module.exports = router;
